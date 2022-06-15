da644b522ba1bb65c7f5c13163db907a77ee8c1a
tophersss <corleone2584@gmail.com>
2022-06-15 11:48:44 +0800

    update Hero banner

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,12 @@
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Avatar from '@mui/material/Avatar';
+import CardHeader from '@mui/material/CardHeader';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import GitHub from '@mui/icons-material/GitHub';
 
 const About = () => {
     return (
@@ -21,8 +25,18 @@ const About = () => {
                     color="text.primary"
                     gutterBottom
                 >
-                    <img src="https://flagicons.lipis.dev/flags/4x3/hk.svg" height="42" style={{borderRadius: '20'}}/>
-                    &nbsp;Cinemas Dashboard
+                    Cinemas
+                    <br/>
+                    Dashboard
+                    <br/>
+                    
+                    <div class="flag-outer-wrapper">
+                        <a class="flag-wrapper">
+                            <i id="hk-flag"/>
+                        </a>
+                    </div>
+                    {/* <img id='hk-flag' src="https://flagicons.lipis.dev/flags/4x3/hk.svg"/> */}
+                    {/* &nbsp;Cinemas Dashboard */}
                 </Typography>
                 <Typography variant="h5" align="center" color="text.secondary" paragraph>
                     An interactive dashboard made with real-world data to visualize cinemas' popular times, sales, and seat popularity.
@@ -34,7 +48,7 @@ const About = () => {
                     justifyContent="center"
                 >
                     <Button variant="contained">Get Started</Button>
-                    <Button variant="outlined">View Code</Button>
+                    <Button variant="outlined">{<> <GitHubIcon/>&nbsp;View Code</>}</Button>
                     <Button variant="outlined">Contact Me</Button>
                 </Stack>
             </Container>
